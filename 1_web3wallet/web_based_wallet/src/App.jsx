b7ecebeb2b17d45c310e0b4b98e1fc0f08685410
sumit <mazumdarsumit3@gmail.com>
2024-10-07 00:38:25 +0530

    Web based wallet that generates both eth and sol wallets

---
+++ 1_web3wallet/web_based_wallet/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./App.css";
+import { generateMnemonic } from "bip39";
+import { SolanaWallet } from "./components/SolanaWallet";
+import { EthWallet } from "./components/EthWallet";
+
+function App() {
+  const [mnemonic, setMnemonic] = useState("");
+
+  return (
+    <>
+      <input type="text" value={mnemonic}></input>
+      <button
+        onClick={async function () {
+          const mn = await generateMnemonic();
+          setMnemonic(mn);
+        }}
+      >
+        Create Seed Phrase
+      </button>
+      <SolanaWallet />
+      <EthWallet />
+    </>
+  );
+}
+
+export default App;
